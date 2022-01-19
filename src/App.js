129db1d9e30fc37ade2ce18cba1440f37541e2bf
Roman Plotnikov <rekss13@gmail.com>
2022-01-19 17:52:39 +0300

    added and fixed react-burger-menu example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Sidebar from './components/Sidebar';
+import "./assets/fonts/font-awesome-4.2.0/css/font-awesome.min.css";
+import "./assets/css/normalize.css";
+import "./assets/css/sidebar.css";
+
+const menus = {
+    slide: { buttonText: 'Slide', items: 1 },
+    stack: { buttonText: 'Stack', items: 1 },
+    elastic: { buttonText: 'Elastic', items: 1 },
+    bubble: { buttonText: 'Bubble', items: 1 },
+    push: { buttonText: 'Push', items: 1 },
+    pushRotate: { buttonText: 'Push Rotate', items: 2 },
+    scaleDown: { buttonText: 'Scale Down', items: 2 },
+    scaleRotate: { buttonText: 'Scale Rotate', items: 2 },
+    fallDown: { buttonText: 'Fall Down', items: 2 },
+    reveal: { buttonText: 'Reveal', items: 1 }
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Sidebar menus={menus} />;
 }
 
 export default App;
